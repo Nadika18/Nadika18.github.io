ce87389a04330de019c1f3e16422b747f465a66a
Nadika18 <56972017+Nadika18@users.noreply.github.com>
2026-02-27 18:00:12 +0000

    Deploying to gh-pages from @ Nadika18/Nadika18.github.io@aa38437e8a1fb5941743b7da99cf5a55a8839063 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Nadika18.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://nadika18.github.io/#contact">contact us</a>.'
               }
             ]
           }
