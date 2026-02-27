a0a2adc261aab8bbbaaaeba486518f4b72a47157
Nadika18 <56972017+Nadika18@users.noreply.github.com>
2026-02-27 17:54:36 +0000

    Deploying to gh-pages from @ Nadika18/Nadika18.github.io@038e49b916470049f5cf38e05637a8ca1cc06403 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Nadika18/homepage",
+    "data-repo": "Nadika18/Nadika18.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
